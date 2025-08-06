c4f9d83801c647725b514b9644787f8763de46c4
sdfgh5343 <sdfgh5343@gmail.com>
2025-08-06 12:13:49 +0200

    Auto update 2025-08-06 12:13:49

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ python3 main.py >> log.txt 2>&1
 
 cd /var/services/homes/administer/Daily-News-Update
 git add .
-git add index.html
+#git add index.html
 git commit -m "Auto update $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main >> gitpush.log 2>&1
 
