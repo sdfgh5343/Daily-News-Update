33f2911b2d317474cc78805268c61473da67ab45
sdfgh5343 <sdfgh5343@gmail.com>
2026-01-29 11:02:54 +0100

    Auto update 2026-01-29 11:02:54

--- Script/main.py
+++ Script/main.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     get_daily_cnn_business_news_txt("../News/cnn_news.txt", limit=20)
     
     for currency in ["USD","JPY","EUR","CNY"]:
-        csv_file = 'Data/Historical Download/{currency}_{t}_Historical.csv'
+        csv_file = '../Data/Historical Download/{currency}_{t}_Historical.csv'
         plot_currency(path=csv_file, currency=currency)
 
         plot_now(csv_file_path =f'../Data/history/{currency}.csv',
